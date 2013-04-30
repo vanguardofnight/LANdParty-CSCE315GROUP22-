00e4a90c34917a2d1a57221bf9f3036b6a9735a2
Colton Travis Revia <crevia1@ginstall.global.cs.tamu.edu>
2013-04-30 15:18:50 -0500

    Committing edit switching index to php, and small changes to facebook_1_pg.

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<p>Hi!</p>
+<p>You can get to php from <a href="http://students.cse.tamu.edu/crevia1/facebook_1_pg.php">here</a><p>
+  <iframe = src="http://www.facebook.com/plugins/like.php?href=http://students.cse.tamu.edu/crevia1/"
+   scrolling = "no" frameborder="0"
+   style="border::none; width:450px; height:80px"><iframe>
+</body>
+</html>
