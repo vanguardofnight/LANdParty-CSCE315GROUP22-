24be1fd6a7f58a0a4764259c2be58d0625e477f1
Colton Travis Revia <crevia1@ginstall.global.cs.tamu.edu>
2013-05-06 17:41:00 -0500

    Adding contact page, with slightly old pictures.

---
+++ contact_us.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>LANd Party- Find nearby games</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<hr>
+
+<body>
+<!--Navigation bar under image -->
+<div id="wrapper">
+  <div id="logo" class="container">
+    <p><a href="http://projects.cse.tamu.edu/crevia1/"><img src="our_images/logo_dbg.gif" alt="Are you game?" width="400" height="300"></a></p>
+  </div>
+  <hr>
+  <div id="menu-wrapper">
+
+    <div id="menu" class="container">
+      <ul>
+	<li><a href="index.php">Home, James</a></li>
+	<li><a href="facebook_1_pg.php">Arr, take me to your bookface</a></li>
+	<li class="current_page_item"><a href="contact_us.php">Contact Us!</a></li>
+      </ul>
+    </div>
+  </div> <!--End of menu-wrapper-->
+
+  <div id="three-column" class="container">
+    <div id="tbox1">
+      <div class="box-style box-style02">
+	<div class="content">
+	  <p><img src="our_images/gaming.jpeg" alt="Find yourself?" align = center height=250px hspace = 10px vspace = 10px>
+	    <h2>Colton Revia</h2>
+	    (Look at that dashing gent, isn't he so suave and elegant?)
+	    email: crevia1@gmail.com
+	    Steam: vanguardofnight
+	  </p>
+	</div>
+      </div>
+    </div> <!--End tbox1-->
+
+    <div id="tbox2">
+      <div class="box-style box-style05">
+	<div class="content">
+	  <p><img src="our_images/IMAG0089.jpg" alt="Find yourself?" align = center height=250px hspace = 10px vspace = 10px>
+	    <h2>Erick Chaves!</h2>
+	    (Actually, Colton again)
+	  </p>
+	</div>
+      </div>
+    </div> <!--End tbox2-->
+
+    <div id="tbox3">
+      <div class="box-style box-style04">
+	<div class="content">
+	  <p><img src="our_images/IMAG0090.jpg" alt="Find yourself?" align = center height=250px hspace = 10px vspace = 10px>
+	    <h2>Kristen Musolf!</h2>
+	    (Eventually)
+	  </p>
+	</div>
+      </div>
+    </div> <!--End tbox3-->
+
+  </div> <!--End three-column-->
+
+
+</div> <!--End of wrapper-->
+
+<div id="page" class="container">
+  <div id = "content">
+    <div class="post">
+	<p> Drop us a line! </p>
+	<iframe = src="http://www.facebook.com/plugins/like.php?href=http://projects.cse.tamu.edu/crevia1/"
+   	  scrolling = "no" frameborder="0"
+   	  style="border::none; width:450px; height:80px"></iframe>
+    </div>
+  </div>
+
+</div>
+
+<!-- signing the page -->
+<div id="footer">
+  <p>
+    Created 1 April 2013, by Colton Revia, Erick Chaves, and Kristen Musolf.  Design template by <a href="http://www.freecsstemplates.org">FCT</a>, background taken from <a href="1-background.com/stars_1.htm">StarFields' backgrounds</a>.
+  </p>
+</div>
+
+
+</body>
+</html>
