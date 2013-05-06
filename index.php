8ac76181e63ae1e5b1c467d679fdb4f53fb8cd84
echaves <erick.chaves@tamu.edu>
2013-05-06 17:50:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,41 @@
 <?php
-	// require_once("facebook.php");
-	// if(is_callable('curl_init'))
-	// {
-		// echo "curl exists";
-	// }
-	// else
-	// {
-		// echo "curl doesn't exist";
-	// }
-	// echo "hello world1";
-	// $facebook = new Facebook(array(
-  // 'appId'  => '136730719847805',
-  // 'secret' => 'eb120b2c585d641cfbbe1b29d3650b08',
-  // 'cookie' => true,
-	// ));
-	// include 'http_build_url';
-	// echo http_build_url("http://user@www.example.com/pub/index.php?a=b#files",
-		// array(
-			// "scheme" => "http",
-			// "host" => "maps.googleapis.com",
-			// "path" => "/maps/api/place/textsearch/",
-			// "query" => "xml?query=gamestores+near+austin&sensor=false&key=AIzaSyBdDviVXZl_XOlkbvuAm5KQ0wRcS1bVVeA"
-			// ),
-			// HTTP_URL_STRIP_AUTH | HTTP_URL_JOIN_PATH | HTTP_URL_JOIN_QUERY | HTTP_URL_STRIP_FRAGMENT
-		// );
-	
-	
-	
-	require_once ("facebook.php");
-	//require_once ("xmlTest.php");
+require_once ("facebook.php");
+require('GoogleGeocode.php');
   
-  
- //echo "hello world2";
+  session_start();
+  $_SESSION = array();
   $config = array(
   'appId' => '136730719847805',
   'secret' => 'eb120b2c585d641cfbbe1b29d3650b08',
   'fileUpload' => true, // optional
+  //'cookie' => true,
   );
-  //echo "config array instantiated";
-  $facebook = new Facebook($config);
-  $user_id = $facebook->getUser();
-  $access_token = $facebook->getAccessToken();
- if ($user_id) {
-    $logoutUrl = $facebook->getLogoutUrl();
-} else {
-    $loginUrl = $facebook->getLoginUrl();
-}
-  
-  //echo "got user ". $user_id;
 
-  ?>
+  $facebook = new Facebook($config);
+  $login_params = array(
+		'scope' => 'user_location, user_about_me',
+		'redirect_uri' => 'http://projects.cse.tamu.edu/chaveser',
+		'display' =>  'page'
+  );
   
+  $login_status_params = array(
+	'ok_session' => 'http://projects.cse.tamu.edu/crevia1',
+	'no_user' => $facebook->getLoginUrl($login_params),
+	'no_session' => $facebook->getLoginUrl($login_params)
+  );
+  $user_id = $facebook->getUser();
+  $access_token = $facebook->getAccessToken;
+  
+  $logout_params = array(
+		'next' =>  'http://projects.cse.tamu.edu/chaveser/'
+  );
+ if ($user_id) {
+    $logoutUrl = $facebook->getLogoutUrl($logout_params);
+} else {
+    $loginUrl = $facebook->getLoginUrl($login_params);
+}
+  ?>
+
   <html>
 	<head></head>
 	<body>
@@ -65,19 +50,26 @@
 			  cookie     : true, // enable cookies to allow the server to access the session
 			  xfbml      : true  // parse XFBML
 			});
-
+			
+			FB.Event.subscribe('auth.login', function(){
+				window.location.reload();
+			});
+			
 					// Additional init code here
 		 // Here we subscribe to the auth.authResponseChange JavaScript Event. This event is fired
 		  // for any auth related change, such as login, logout or session refresh. This means that
 		  // whenever someone who was previously logged out then logs in, the correct case below 
 		  // will be handled. 
+		  //window.location.reload();
 		  FB.Event.subscribe('auth.authResponseChange', function(response) {
+			
 			// Here we specify what we do with the response anytime this event occurs. 
 			if (response.status === 'connected') {
 			  // The response object is returned with a status field that lets us know what the current
 			  // login status of the person is. In this case, we're handling the situation where they 
 			  // have logged in to the app.
-			  testAPI();
+			  //testAPI();
+			 
 			} else if (response.status === 'not_authorized') {
 			  // In this case, the person is logged into Facebook, but not into the app, so we call
 			  // FB.login() to prompt them to do so. 
@@ -87,6 +79,7 @@
 			  // result from direct user interaction (such as a mouse click)
 			  // (2) it is a bad experience to be continually prompted to login upon page load.
 			  FB.login();
+			  
 			} else {
 			  // In this case, the person is not logged into Facebook, so we call the login() 
 			  // function to prompt them to do so. Note that at this stage there is no indication
@@ -94,6 +87,7 @@
 			  // Dialog right after they login to Facebook. 
 			  // The same caveats as above apply to the FB.login() call here.
 			  FB.login();
+			  
 			}
 		  });
 		  };
@@ -107,7 +101,17 @@
 			 js.src = "//connect.facebook.net/en_US/all.js";
 			 ref.parentNode.insertBefore(js, ref);
 		   }(document));
-		</script>
+		// Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+</script>
+</body>
+</html>
 		
 	<?php
   
@@ -136,17 +140,13 @@
 		echo $e->getTraceAsString();
 		echo "<br />";
 		echo "<br />";
-		$login_url = $facebook->getLoginUrl(); 
         echo 'Please <a href="' . $login_url . '">login 1.</a>';
         error_log($e->getType());
         error_log($e->getMessage());
 		}
 	} else {
 	// No user, so print a link for the user to login
-      $login_url = $facebook->getLoginUrl();
-      echo 'Please <a href="' . $login_url . '">login.</a>';
-	  //$me = $facebook->api('/me');
-	  //echo "me = " . $me;
+      $login_url = $facebook->getLoginUrl($login_params);
 	}
 	//Save to XML
 	$locations_array = array();
@@ -157,7 +157,7 @@
 	
 	$doc = new DOMDocument();
 	$doc ->formatOutput = true;
-	
+	$preserveWhiteSpace = true;
 	$r = $doc->createElement( "locations" );
 	$doc-> appendChild($r);
 	
@@ -169,8 +169,8 @@
 	$b->appendChild($name);
 	$r->appendChild($b);
 	
-	$b = $doc->createElement("location");
-	$location = $doc->createElement("location");
+	$b = $doc->createElement("location1");
+	$location = $doc->createElement("location2");
 	$location -> appendChild($doc->createTextNode($locs['location']));
 	$b->appendChild($location);
 	$r->appendChild($b);
@@ -184,33 +184,71 @@
 	
 	$locations_array = $doc->getElementsByTagName("locations");
 	foreach($locations_array as $locs){
-		$locations = $locs->getElementsByTagName("location");
+		$locations = $locs->getElementsByTagName("location1");
 		$location = $locations->item(0)->nodeValue;
-		
-		echo "location : " . $location;
 	}
+	function replace_whitespaces($string){
+		$new_location = preg_replace('/\s+/', '+',$string);
+		return $new_location;
+	}
+	$new_location = replace_whitespaces($location);
+	$url = "https://maps.googleapis.com/maps/api/place/textsearch/xml";
+	$search_variable = "video+game+stores+near";
+	$api_key = "&sensor=false&key=AIzaSyDhr8wxu4MLOEqivMJc-E16vGxM_qhtgZk";
+	$new_url = $url . "?query=$search_variable$new_location&radius=50" . $api_key;
+	//$_SESSION['new_url']=$new_url;
 	
+	$current_file = file_get_contents($new_url);
+	file_put_contents('gamestores.xml', $current_file);
 	
-// if(file_exists('locations.xml')){
-	// $xml = new DOMDocument();
-	// $xml->load("locations.xml");
-	// $xml->formatOutput = true;
-	// $xml_user = $xml->createElement("User");
-	// $xml_location = $xml->createElement("Location");
-	// $xml_user->appendChild( $xml_location );
-	// $xml->appendChild( $xml_user );
-	// $xml->save("locations.xml");
-// }else{
-	// $xml = new DOMDocument();
-	// $xml->formatOutput = true;
-	// $xml_user = $xml->createElement("User");
-	// $xml_location = $xml->createElement("Location");
-	// $xml_user->appendChild( $xml_location );
-	// $xml->appendChild( $xml_user );
-	// $xml->save("locations.xml");
-    
-//}
-
+	$doc = new DOMDocument();
+	$doc->load('gamestores.xml');
+	
+	$names_array = array();
+	$addr_array = array();
+	$lat_array = array();
+	$lng_array = array();
+	$results = $doc->getElementsByTagName("result");
+	foreach($results as $res){
+		$names = $res->getElementsByTagName("name");
+		$name = $names->item(0)->nodeValue;
+		
+		$addresses = $res->getElementsByTagName("formatted_address");
+		$address = $addresses->item(0)->nodeValue;
+		
+		$lats = $res->getElementsByTagName("lat");
+		$lat = $lats->item(0)->nodeValue;
+		
+		$lngs = $res->getElementsByTagName("lng");
+		$lng = $lngs->item(0)->nodeValue;
+		
+		$names_array [] = $name;
+		$addr_array [] = $address;
+		$lat_array [] = $lat;
+		$lng_array [] = $lng;	
+	}
+	$center_lat = $lat_array[0];
+	$center_lng = $lng_array[0];
+	echo $center_lat;
+	echo $center_lng;
+	session_start();
+	$_SESSION['cent_lat']=$center_lat;
+	$_SESSION['cent_lng']=$center_lng;
+	session_write_close();
+	$markerDoc = new DOMDocument();
+	$node = $markerDoc->createElement("markers");
+	$parnode = $markerDoc->appendChild($node);
+	$size = sizeof($names_array);
+	
+	for($i=0; $i<$size; $i++){
+		$node = $markerDoc->createElement("marker");
+		$newnode = $parnode->appendChild($node);
+		$newnode->setAttribute("name", $names_array[$i]);
+		$newnode->setAttribute("address", $addr_array[$i]);
+		$newnode->setAttribute("lat", $lat_array[$i]);
+		$newnode->setAttribute("lng", $lng_array[$i]);
+	}
+	$markerDoc->save("markerDoc.xml");
   ?>
 	</body>
 </html>
@@ -223,174 +261,22 @@
  <body>
  <?php if ($user_id): ?>
     <?php echo "Welcome, ".$user_name. ".<br />"; ?>
-    <a href="<?php echo $logoutUrl; ?>">
+    <a href="<?php echo $logoutUrl;?>">
       <img src="http://www.picturehealing.com/dashboard/app/Assets/fblogout.png">
     </a>
     <?php else: ?>
-      <a href="<?php echo $loginUrl; ?>">
+      <a href="<?php session_destroy();
+	  echo $loginUrl; ?>">
         <img src="http://www.challengeyoursoul.com/images/facebooklogin.png">
       </a>
     <?php endif ?>
+	
   <!-- <p> Run xmlTest <a href = "http://projects.cse.tamu.edu/chaveser/xmlTest.php">here</a><p> -->
  <p>You can get to php from <a href="http://projects.cse.tamu.edu/crevia1/facebook_1_pg.php">here</a><p>
+ <p>You can get to the map from <a href="http://projects.cse.tamu.edu/chaveser/searchResults2.php">here</a><p>
   <iframe = src="http://www.facebook.com/plugins/like.php?href=http://students.cse.tamu.edu/crevia1/"
    scrolling = "no" frameborder="0"
    style="border::none; width:450px; height:80px"><iframe>
+	
  </body>
 </html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <title>Google Maps AJAX + mySQL/PHP Example</title>
-    <script src="http://maps.googleapis.com/maps/api/js?sensor=false"
-            type="text/javascript"></script>
-    <script type="text/javascript">
-    //<![CDATA[
-    var map;
-    var markers = [];
-    var infoWindow;
-    var locationSelect;
-
-    function load() {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(25, 100),
-        zoom: 4,
-        mapTypeId: 'roadmap',
-        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-      });
-      infoWindow = new google.maps.InfoWindow();
-
-      locationSelect = document.getElementById("locationSelect");
-      locationSelect.onchange = function() {
-        var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-        if (markerNum != "none"){
-          google.maps.event.trigger(markers[markerNum], 'click');
-        }
-      };
-   }
-
-   function searchLocations() {
-     var address = document.getElementById("addressInput").value;
-     var geocoder = new google.maps.Geocoder();
-     geocoder.geocode({address: address}, function(results, status) {
-       if (status == google.maps.GeocoderStatus.OK) {
-        searchLocationsNear(results[0].geometry.location);
-       } else {
-         alert(address + ' not found');
-       }
-     });
-   }
-
-   function clearLocations() {
-     infoWindow.close();
-     for (var i = 0; i < markers.length; i++) {
-       markers[i].setMap(null);
-     }
-     markers.length = 0;
-
-     locationSelect.innerHTML = "";
-     var option = document.createElement("option");
-     option.value = "none";
-     option.innerHTML = "See all results:";
-     locationSelect.appendChild(option);
-   }
-
-   function searchLocationsNear(center) {
-     clearLocations();
-
-     var radius = document.getElementById('radiusSelect').value;
-     var searchUrl = 'phpsqlsearch_genxml.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
-     downloadUrl(searchUrl, function(data) {
-       var xml = parseXml(data);
-       var markerNodes = xml.documentElement.getElementsByTagName("marker");
-       var bounds = new google.maps.LatLngBounds();
-       for (var i = 0; i < markerNodes.length; i++) {
-         var name = markerNodes[i].getAttribute("name");
-         var address = markerNodes[i].getAttribute("address");
-         var distance = parseFloat(markerNodes[i].getAttribute("distance"));
-         var latlng = new google.maps.LatLng(
-              parseFloat(markerNodes[i].getAttribute("lat")),
-              parseFloat(markerNodes[i].getAttribute("lng")));
-
-         createOption(name, distance, i);
-         createMarker(latlng, name, address);
-         bounds.extend(latlng);
-       }
-       map.fitBounds(bounds);
-       locationSelect.style.visibility = "visible";
-       locationSelect.onchange = function() {
-         var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-         google.maps.event.trigger(markers[markerNum], 'click');
-       };
-      });
-    }
-
-    function createMarker(latlng, name, address) {
-      var html = "<b>" + name + "</b> <br/>" + address;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: latlng
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(html);
-        infoWindow.open(map, marker);
-      });
-      markers.push(marker);
-    }
-
-    function createOption(name, distance, num) {
-      var option = document.createElement("option");
-      option.value = num;
-      option.innerHTML = name + "(" + distance.toFixed(1) + ")";
-      locationSelect.appendChild(option);
-    }
-
-    function downloadUrl(url, callback) {
-      var request = window.ActiveXObject ?
-          new ActiveXObject('Microsoft.XMLHTTP') :
-          new XMLHttpRequest;
-
-      request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-          request.onreadystatechange = doNothing;
-          callback(request.responseText, request.status);
-        }
-      };
-
-      request.open('GET', url, true);
-      request.send(null);
-    }
-
-    function parseXml(str) {
-      if (window.ActiveXObject) {
-        var doc = new ActiveXObject('Microsoft.XMLDOM');
-        doc.loadXML(str);
-        return doc;
-      } else if (window.DOMParser) {
-        return (new DOMParser).parseFromString(str, 'text/xml');
-      }
-    }
-
-    function doNothing() {}
-
-    //]]>
-  </script>
-  </head>
-
-  <body style="margin:0px; padding:0px;" onload="load()">
-    <div>
-     <input type="text" id="addressInput" size="10"/>
-    <select id="radiusSelect">
-      <option value="25" selected>25mi</option>
-      <option value="100">100mi</option>
-      <option value="200">200mi</option>
-    </select>
-
-    <input type="button" onclick="searchLocations()" value="Search"/>
-    </div>
-    <div><select id="locationSelect" style="width:100%;visibility:hidden"></select></div>
-    <div id="map" style="width: 100%; height: 80%"></div>
-  </body>
-</html>
-
